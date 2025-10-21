ef6b95f3fabd77724ef5dfb1c1bd0dfd4586cf7c
kdjxs <darrensfang143@gmail.com>
2025-10-21 03:14:24 -0700

    Step 8 complete, added multiple sticker buttons

--- src/main.ts
+++ src/main.ts
@@ -44,7 +44,7 @@ class MarkerPreview implements PreviewCommand {
     ctx.globalAlpha = 0.4;
     ctx.beginPath();
     ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
-    ctx.fillStyle = 'black';
+    ctx.fillStyle = "black";
     ctx.fill();
     ctx.restore();
   }
@@ -119,9 +119,7 @@ let brushSize = 2;
 let curTool: "marker" | "sticker" = "marker";
 let currentPreview: PreviewCommand | null = null;
 
-
 currentPreview = new MarkerPreview(brushSize / 2);
-
 
 stickers.forEach((emoji) => {
   const btn = document.createElement("button");
